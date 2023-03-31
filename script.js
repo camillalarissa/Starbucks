276524f3d78d6a3bc69f094401a2dfb510a37ea0
camillalarissa <115382914+camillalarissa@users.noreply.github.com>
2023-03-31 17:29:07 -0300

    Starbucks

---
+++ script.js
@@ -0,0 +1,12 @@
+let imagem = document.querySelector(".starbuck")
+let circulo = document.querySelector(".circulo")
+
+function trocaimagem(endereco) {
+
+    imagem.src = endereco
+}
+function trocacor(cor) {
+
+    circulo.style.background = cor
+}
+
